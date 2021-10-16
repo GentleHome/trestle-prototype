b945291d5d6513dd4fc86c027490fafc337feddd
GentleHome <kimberlysoriano144@gmail.com>
2021-10-16 10:42:46 +0800

    week completed

--- sample.js
+++ sample.js
@@ -16,11 +16,14 @@ const date_today = {
     current_month: date.getMonth(),
     current_day: date.getDay(),
     current_date: date.getDate(),
+    current_week_number: getWeekNumber(date.getFullYear(), date.getMonth()),
 };
 
 let manipulate = {
     year: date_today.current_year,
     month: date_today.current_month,
+    date: date_today.current_date,
+    weekNumber: getWeekNumber(date_today.current_year, date_today.current_month),
 };
 
 // initial load out
@@ -40,6 +43,7 @@ function go_choose_view() {
     // reset every view change
     manipulate.year = date_today.current_year;
     manipulate.month = date_today.current_month;
+    manipulate.weekNumber = getWeekNumber(date_today.current_year, date_today.current_month);
 
     renderer(options);
 }
@@ -59,10 +63,8 @@ function renderer(options) {
             break;
 
         case "2":
-            renderBoxes_week(box_range[options], 
-                date_today.current_year, 
-                date_today.current_month,
-                date_today.current_date);
+            let weeks_to_render = getWeeksToRender_today();
+            renderBoxes_week(weeks_to_render);
             break;
 
         case "3":
@@ -96,6 +98,8 @@ function prev_manipulator() {
             break;
 
         case "2":
+            let weeks_to_render = getWeeksToRender_prev();
+            renderBoxes_week(weeks_to_render);
             break;
 
         case "3":
@@ -128,6 +132,8 @@ function next_manipulator() {
             break;
 
         case "2":
+            let weeks_to_render = getWeeksToRender_next();
+            renderBoxes_week( weeks_to_render);
             break;
 
         case "3":
@@ -153,6 +159,8 @@ function today_manipulator() {
             break;
 
         case "2":
+            let weeks_to_render = getWeeksToRender_today();
+            renderBoxes_week(weeks_to_render);
             break;
 
         case "3":
@@ -208,6 +216,10 @@ function renderBoxes_year(range, year) {
 				daycounter += 1;
 				if (daycounter == date.getDate() && year == date.getFullYear() && month == date.getMonth()) {
 					html += '<span class="small_box-2">' + daycounter + "</span>";
+                    if (br2 == 7) {
+                        html += "<br>";
+                        br2 = 0;
+                    }
 					daycounter += 1;
 					index++;
 					br2 += 1;
@@ -273,9 +285,14 @@ function renderBoxes_month(range, month, year) {
             daycounter += 1;
             if (daycounter == date.getDate() && year == date.getFullYear() && month == date.getMonth()) {
                 html += '<span class="box2">' + daycounter + "</span>";
+                if (br == 7) {
+                    html += "<br>";
+                    br = 0;
+                }
                 daycounter += 1;
                 index++;
                 br += 1;
+                
             }
             html += '<span class="box">' + daycounter + "</span>";
             if (br == 7) {
@@ -297,17 +314,57 @@ function renderBoxes_month(range, month, year) {
     document.querySelector(".container").innerHTML = html;
 }
 
-function renderBoxes_week(range, year, month, date) {
+function renderBoxes_week(weeks_to_render) {
+    let mydate = new Date(date.getFullYear(), weeks_to_render.month);
+    let month_name = mydate.toLocaleString("default", { month: "long" });
+    date_indication.innerHTML = month_name + "-" + date.getFullYear();
     let html = "";
-    let mydate = new Date(year, month, date);
-    let myday = mydate.getDay();
-    for (let index = 0; index < day.length; index++) {
+    let before = weeks_to_render.before;
+    let current = weeks_to_render.current;
+    let after = weeks_to_render.after;
+    let month = weeks_to_render.month;
+
+    let before_size = before.length;
+    let current_size = current.length;
+    let after_size = after.length;
+    let index = 0;
+
+    for (let index = 0; index < day.length; index++) { 
         html += '<span class="long_box-day">' + small_day[index] + "</span>";
     }
     html += '<br>';
+    if(current.length < 7){
+        if(current[0] == 1){
+            while(before_size != 0){
+                html += '<span class="long_box-3">'+ before[index] +'</span>';
+                before_size-=1;
+                index++;
+            }
+        }
+    }
+    index = 0;
+    while(index != current_size){
+        if(current[index] == date.getDate() && month == date.getMonth()){
+            html += '<span class="long_box-2">'+ current[index] +'</span>';
+            if(index+=1 == RangeError){
+                break;
+            }else{
+                index+=1;
+            }
+        }
+        html += '<span class="long_box">'+ current[index] +'</span>';
+        index++;
+    }
+    index = 0;
 
-    for (let index = 0; index < range; index++) {
-        html += '<span class="long_box"></span>';
+    if(current.length < 7){
+        if(current[0] > 1){
+            while(after_size != 0){
+                html += '<span class="long_box-3">'+ after[index] +'</span>';
+                after_size-=1;
+                index++;
+            }
+        }
     }
     document.querySelector(".container").innerHTML = html;
 }
@@ -342,4 +399,143 @@ function daysInMonth(month, year) {
 
 function getDayStartOfMonth(month, year) {
     return new Date(year, month, 1).getDay();
+}
+
+// code from https://gist.github.com/markthiessen/3883242
+function getWeeksInMonth(year, month) {
+    const weeks = [],
+      firstDate = new Date(year, month, 1),
+      lastDate = new Date(year, month + 1, 0),
+      numDays = lastDate.getDate();
+  
+    let dayOfWeekCounter = firstDate.getDay();
+  
+    for (let date = 1; date <= numDays; date++) {
+      if (dayOfWeekCounter === 0 || weeks.length === 0) {
+        weeks.push([]);
+      }
+      weeks[weeks.length - 1].push(date);
+      dayOfWeekCounter = (dayOfWeekCounter + 1) % 7;
+    }
+
+    return weeks;
+}
+
+// takes year and month, feeds myweeks with the 2D array getWeeksInMonth() returns
+// loops myweeks and tries to find what index is the date today and returns what row number it is in the array
+function getWeekNumber(year, month){
+    let myweeks = getWeeksInMonth(year, month);
+    for (let i = 0; i < myweeks.length; i++) {
+        if(myweeks[i].includes(date.getDate())){
+            return i;
+        }
+    }
+}
+
+function getWeeksToRender_prev() {
+    manipulate.weekNumber -= 1;
+    let weekNumber_before = manipulate.weekNumber - 1;
+    let month_before = manipulate.month;
+    let weekNumber_after = manipulate.weekNumber + 1;
+    let month_after = manipulate.month;
+
+    if (weekNumber_before < 0) {
+        month_before -= 1;
+        weekNumber_before = getWeeksInMonth(manipulate.year, month_before).length - 1;
+    }
+
+    if (manipulate.weekNumber < 0) {
+        manipulate.month -= 1;
+        manipulate.weekNumber = getWeeksInMonth(manipulate.year, manipulate.month).length - 1;
+    }
+
+    if (weekNumber_after > getWeeksInMonth(manipulate.year, manipulate.month).length-1) {
+        month_after += 1;
+        weekNumber_after = 0;
+    }
+
+    if(manipulate.month < 0){
+        manipulate.month = 0;
+        manipulate.weekNumber = 0;
+    }
+
+    let weeks_to_render_before = getWeeksInMonth(manipulate.year, month_before)[weekNumber_before];
+    let weeks_to_render = getWeeksInMonth(manipulate.year, manipulate.month)[manipulate.weekNumber];
+    let weeks_to_render_after = getWeeksInMonth(manipulate.year, month_after)[weekNumber_after];
+
+    return weeks_to_render = {
+        before: weeks_to_render_before,
+        current: weeks_to_render,
+        after: weeks_to_render_after,
+        month: manipulate.month,
+    };
+}
+
+function getWeeksToRender_next() {
+    manipulate.weekNumber += 1;
+    let weekNumber_before = manipulate.weekNumber - 1;
+    let month_before = manipulate.month;
+    let weekNumber_after = manipulate.weekNumber + 1;
+    let month_after = manipulate.month;
+
+    if (weekNumber_before < 0) {
+        month_before -= 1;
+        weekNumber_before = getWeeksInMonth(manipulate.year, month_before).length - 1;
+    }
+
+    if (manipulate.weekNumber > getWeeksInMonth(manipulate.year, manipulate.month).length - 1) {
+        manipulate.month += 1;
+        manipulate.weekNumber = 0;
+    }
+
+    if (weekNumber_after > getWeeksInMonth(manipulate.year, manipulate.month).length-1) {
+        month_after += 1;
+        weekNumber_after = 0;
+    }
+
+    if(manipulate.month > 11){
+        manipulate.month = 11;
+        manipulate.weekNumber = getWeeksInMonth(manipulate.year, manipulate.month).length - 1
+    }
+
+    let weeks_to_render_before = getWeeksInMonth(manipulate.year, month_before)[weekNumber_before];
+    let weeks_to_render = getWeeksInMonth(manipulate.year, manipulate.month)[manipulate.weekNumber];
+    let weeks_to_render_after = getWeeksInMonth(manipulate.year, month_after)[weekNumber_after];
+
+    return weeks_to_render = {
+        before: weeks_to_render_before,
+        current: weeks_to_render,
+        after: weeks_to_render_after,
+        month: manipulate.month,
+    };
+}
+
+function getWeeksToRender_today(){
+    manipulate.month = date_today.current_month;
+    manipulate.weekNumber = date_today.current_week_number;
+    let weekNumber_before = date_today.current_week_number - 1;
+    let month_before = date_today.current_month-1;
+    let weekNumber_after = date_today.current_week_number + 1;
+    let month_after = date_today.current_month;
+
+    if (weekNumber_before < 0) {
+        month_before -= 1;
+        weekNumber_before = getWeeksInMonth(date_today.current_year, month_before).length - 1;
+    }
+
+    if (weekNumber_after > getWeeksInMonth(date_today.current_year, month_after).length-1) {
+        month_after += 1;
+        weekNumber_after = 0;
+    }
+
+    let weeks_to_render_before = getWeeksInMonth(date_today.current_year, month_before)[weekNumber_before];
+    let weeks_to_render = getWeeksInMonth(date_today.current_year, date_today.current_month)[date_today.current_week_number];
+    let weeks_to_render_after = getWeeksInMonth(date_today.current_year, month_after)[weekNumber_after];
+
+    return weeks_to_render = {
+        before: weeks_to_render_before,
+        current: weeks_to_render,
+        after: weeks_to_render_after,
+        month: date_today.current_month,
+    };
 }
