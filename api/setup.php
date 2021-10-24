0572384c36777c28c0295532dff14097a687c044
YourAlly <Royettemiranda@YAHOO.COM>
2021-10-25 00:21:21 +0800

    Edited a directory string

--- api/setup.php
+++ api/setup.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__) . '\..\vendor\autoload.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 require_once dirname(__FILE__) . './helpers/constants.php';
 include dirname(__FILE__) . './helpers/parsers.php';
 
