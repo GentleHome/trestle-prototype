ad8b987614533263096abee7b0917f653df34a35
YourAlly <Royettemiranda@YAHOO.COM>
2021-11-07 22:28:24 +0800

    sa

--- src/Reminders.php
+++ src/Reminders.php
@@ -40,13 +40,13 @@ class Reminder
     private $message;
 
     /**
-     * @ORM\Column(type="datetime", nullable=false)
+     * @ORM\Column(type="datetime", nullable=false, options={"default": "CURRENT_TIMESTAMP"})
      * @var DateTime
      */
     private $date_created;
 
     /**
-     * @ORM\Column(type="datetime", nullable=false)
+     * @ORM\Column(type="datetime", nullable=false, options={"default": "CURRENT_TIMESTAMP"})
      * @var DateTime
      */
     private $remind_date;
@@ -70,7 +70,6 @@ class Reminder
     private $is_recurring;
 
 
-
     // Class Methods
 
     /**
@@ -81,5 +80,74 @@ class Reminder
         return $this->id;
     }
 
-    
+    /**
+     * Returns the owner of the reminder.
+     */
+    public function get_user(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * Sets the owner of the reminder.
+     */
+    public function set_user(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function get_type(): string
+    {
+        return $this->type;
+    }
+
+    public function set_type($type): void
+    {
+        $this->type = $type;
+    }
+
+    public function get_title(): int
+    {
+        return $this->title;
+    }
+
+    public function set_title($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function get_message(): string
+    {
+        return $this->message;
+    }
+
+    public function set_message($message): void
+    {
+        $this->message = $message;
+    }
+
+    public function get_date_created(): DateTime
+    {
+        return $this->date_created;
+    }
+
+    public function get_remind_date(): DateTime
+    {
+        return $this->remind_date;
+    }
+
+    public function set_remind_date($remind_date): void
+    {
+        $this->remind_date = $remind_date;
+    }
+
+    public function get_is_checked(): bool
+    {
+        return $this->is_checked;
+    }
+
+    public function set_is_checked($is_checked): void
+    {
+        $this->is_checked = $is_checked;
+    }
 }
