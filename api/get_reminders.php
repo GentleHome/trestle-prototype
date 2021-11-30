ec9a1400665b0894a2c2fe88262bb11a759be42b
YourAlly <Royettemiranda@YAHOO.COM>
2021-11-30 19:10:25 +0800

    Changed get_reminders to filter by is_recurring

--- api/get_reminders.php
+++ api/get_reminders.php
@@ -7,27 +7,28 @@ $errors = array("errors" => []);
 if (!isset($_SESSION[USER_ID]) && !TEST_MODE) {
 
     array_push($errors["errors"], ERROR_MISSING_LOGGED_IN_USER);
+    
 }
 
 if (empty($errors['errors'])) {
+
     $type = null;
+    $is_recurring = null;
+    $lookups = array();
     $user_id = !TEST_MODE ? $_SESSION[USER_ID] : 1;
 
     $user = $entityManager->find("User", $user_id);
+    $lookups['user'] = $user;
 
-    if(isset($_GET['type'])){
-
-        $type = $_GET['type'];
-
+    if (isset($_GET['type'])) {
+        $lookups['type'] = $_GET['type'];
     }
 
-    if ($type == TYPE_TASK || $type == TYPE_REMINDER) {
-
-        $reminders = $entityManager->getRepository("Reminder")->findBy(array('user' => $user, "type" => $type));
-    } else {
-
-        $reminders = $entityManager->getRepository("Reminder")->findBy(array('user' => $user));
+    if (isset($_GET['is_recurring'])) {
+        $lookups['is_recurring'] = $_GET['is_recurring'];
     }
+
+    $reminders = $entityManager->getRepository("Reminder")->findBy($lookups);
 
     $response = [];
     foreach ($reminders as $reminder) {
@@ -37,5 +38,7 @@ if (empty($errors['errors'])) {
     echo json_encode($response);
 
 } else {
+
     echo json_encode($errors);
+
 }
