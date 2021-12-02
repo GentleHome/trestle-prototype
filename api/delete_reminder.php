ba99ea1dd7b936615c88ab5bc539acc83e04834b
YourAlly <Royettemiranda@YAHOO.COM>
2021-12-02 22:45:14 +0800

    Updated test.php to cover reminders apis

--- api/delete_reminder.php
+++ api/delete_reminder.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION[USER_ID]) && !TEST_MODE) {
     array_push($errors["errors"], ERROR_MISSING_LOGGED_IN_USER);
 }
 
-if (!isset($_POST['reminder_id'])) {
+if (!isset($_POST['reminder-id'])) {
 
     array_push($errors["errors"], ERROR_MISSING_VALUE . ": Reminder");
 }
@@ -20,19 +20,21 @@ if (empty($errors['errors'])) {
 
     $lookups = array();
     $user_id = !TEST_MODE ? $_SESSION[USER_ID] : 1;
-    $reminder_id = $_POST['reminder_id'];
+    $reminder_id = $_POST['reminder-id'];
 
     $user = $entityManager->find("User", $user_id);
+    $reminder = $entityManager->find("Reminder", $reminder_id);
 
-    $lookups["id"] = $id;
-    $lookups["user"] = $user;
-
-    $reminder = $entityManager->getRepository("Reminder")->findOneBy($lookups);
-
-    echo json_encode(array("success" => "Reminder deleted for " . $remind_date->format("M-d-Y h:i"), "reminder" => parse_reminder($reminder)));
+    if (!$reminder->get_user()->get_id() == $user->get_id()) {
+        array_push($errors["errors"], ERROR_INVALID_ACCESS);
+        echo json_encode($errors);
+        exit;
+    }
 
     $entityManager->remove($reminder);
+    echo json_encode(array("success" => "Reminder deleted for " . $reminder->get_remind_date()->format("M-d-Y h:i"), "reminder" => parse_reminder($reminder)));
     $entityManager->flush();
+
 
 } else {
 
--- api/test-buttons.js
+++ api/test-buttons.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function setButtons() {
+
     // Get Courses
     const getCourses = document.querySelector('#get-courses-button');
     getCourses.addEventListener('click', () => {
@@ -26,7 +27,7 @@ function setButtons() {
     const getAllCourseworks = document.querySelector('#get-all-courseworks-button');
     getAllCourseworks.addEventListener('click', () => {
         const textArea = document.querySelector('#get-all-courseworks-response');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
         
         req = new Request(
@@ -46,7 +47,7 @@ function setButtons() {
     const getAllAnnouncements = document.querySelector('#get-all-announcements-button');
     getAllAnnouncements.addEventListener('click', () => {
         const textArea = document.querySelector('#get-all-announcements-response');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
 
         req = new Request(
@@ -66,20 +67,14 @@ function setButtons() {
     const getCourse = document.querySelector('#get-course-button');
     getCourse.addEventListener('click', () => {
         const textArea = document.querySelector('#get-course-response');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
-        if (selectedCourse.source == "CANVAS") {
-            req = new Request(
-                `get_course.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
-        else if (selectedCourse.source == "GCLASS") {
-            req = new Request(
-                `get_course.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
+
+        req = new Request(
+            `get_course.php?source=${selectedCourse.source}` +
+            `&course_id=${selectedCourse.courseId}`
+        );
+
         fetch(req)
             .then((res) => res.text())
             .then((data) => {
@@ -93,20 +88,13 @@ function setButtons() {
     const getCourseworks = document.querySelector('#get-courseworks-button');
     getCourseworks.addEventListener('click', () => {
         const textArea = document.querySelector('#get-courseworks-response');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
-        if (selectedCourse.source == "CANVAS") {
-            req = new Request(
-                `get_courseworks.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
-        else if (selectedCourse.source == "GCLASS") {
-            req = new Request(
-                `get_courseworks.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
+
+        req = new Request(
+            `get_courseworks.php?source=${selectedCourse.source}` +
+            `&course_id=${selectedCourse.courseId}`
+        );
 
         fetch(req)
             .then((res) => res.text())
@@ -123,25 +111,15 @@ function setButtons() {
         const textArea = document.querySelector('#get-coursework-response');
         const assignIdInput = document.querySelector('#get-coursework-id');
         const typeInput = document.querySelector('#get-coursework-type-option');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
-        if (selectedCourse.source == "CANVAS") {
-            req = new Request(
-                `get_coursework.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}` +
-                `&coursework_id=${assignIdInput.value}` +
-                `&type=${typeInput.value}`
-            );
-        }
 
-        else if (selectedCourse.source == "GCLASS") {
-            req = new Request(
-                `get_coursework.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}` +
-                `&coursework_id=${assignIdInput.value}` +
-                `&type=${typeInput.value}`
-            );
-        }
+        req = new Request(
+            `get_coursework.php?source=${selectedCourse.source}` +
+            `&course_id=${selectedCourse.courseId}` +
+            `&coursework_id=${assignIdInput.value}` +
+            `&type=${typeInput.value}`
+        );
 
         fetch(req)
             .then((res) => res.text())
@@ -156,20 +134,13 @@ function setButtons() {
     const getAnnouncements = document.querySelector('#get-announcements-button');
     getAnnouncements.addEventListener('click', () => {
         const textArea = document.querySelector('#get-announcements-response');
-        const selectedCourse = getCourseIdAndOption();
+        const selectedCourse = getSelectedCourse();
         var req;
-        if (selectedCourse.source == "CANVAS") {
-            req = new Request(
-                `get_announcements.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
-        else if (selectedCourse.source == "GCLASS") {
-            req = new Request(
-                `get_announcements.php?source=${selectedCourse.source}` +
-                `&course_id=${selectedCourse.courseId}`
-            );
-        }
+
+        req = new Request(
+            `get_announcements.php?source=${selectedCourse.source}` +
+            `&course_id=${selectedCourse.courseId}`
+        );
 
        fetch(req)
             .then((res) => res.text())
@@ -180,15 +151,128 @@ function setButtons() {
             });
     });
 
+    // Get Reminders
+    const getReminders = document.querySelector('#get-reminders-button');
+    getReminders.addEventListener('click', () => {
+        const textArea = document.querySelector('#get-reminders-response');
+        const selected = getValueFromInput("#get-reminder-type-option");
+        var req; 
+        
+        req = new Request(
+            `get_reminders.php?type=${selected}`
+        );
+
+        fetch(req)
+            .then((res) => res.text())
+            .then((data) => {
+                textArea.value = data;
+                var parsed = JSON.parse(data);
+                console.log(parsed);
+            });
+    });
+
+    // Post Reminder
+    const postReminder = document.querySelector('#post-reminder-button');
+    postReminder.addEventListener('click', () => {
+        const textArea = document.querySelector('#post-reminder-response');
+        var req;
+
+        const form = document.querySelector("#post-reminder-form");
+        const formData = new URLSearchParams();
+
+        for (const pair of new FormData(form)) {
+            formData.append(pair[0], pair[1]);
+        }
+
+        var data = {
+                method: 'POST',
+                body: formData
+        };
+
+        req = new Request('post_reminder.php', data);
+
+        fetch(req)
+            .then((res) => res.text())
+            .then((data) => {
+                textArea.value = data;
+                var parsed = JSON.parse(data);
+                console.log(parsed);
+            });
+    });
+
+    // Edit Reminder
+    const editReminder = document.querySelector('#edit-reminder-button');
+    editReminder.addEventListener('click', () => {
+        const textArea = document.querySelector('#edit-reminder-response');
+        var req;
+
+        const form = document.querySelector("#edit-reminder-form");
+        const formData = new URLSearchParams();
+
+        for (const pair of new FormData(form)) {
+            formData.append(pair[0], pair[1]);
+        }
+
+        var data = {
+            method: 'POST',
+            body: formData
+        };
+
+        req = new Request('edit_reminder.php', data);
+
+        fetch(req)
+            .then((res) => res.text())
+            .then((data) => {
+                textArea.value = data;
+                var parsed = JSON.parse(data);
+                console.log(parsed);
+            });
+    });
+
+    // Delete Reminder
+    const deleteReminders = document.querySelector('#delete-reminder-button');
+    deleteReminders.addEventListener('click', () => {
+        const textArea = document.querySelector('#delete-reminder-response');
+        var req;
+
+        const form = document.querySelector("#delete-reminder-form");
+        const formData = new URLSearchParams();
+
+        for (const pair of new FormData(form)) {
+            formData.append(pair[0], pair[1]);
+        }
+
+        var data = {
+            method: 'POST',
+            body: formData
+        };
+
+        req = new Request('delete_reminder.php', data);
+
+        fetch(req)
+            .then((res) => res.text())
+            .then((data) => {
+                textArea.value = data;
+                var parsed = JSON.parse(data);
+                console.log(parsed);
+            });
+    });
 }
 
-function getCourseIdAndOption() {
+function getSelectedCourse() {
 
     const courseId = document.querySelector('#get-course-id');
-    const selected = document.querySelector('#get-source-option');
+    const selected = getValueFromInput('#get-source-option');
 
     return {
         courseId: courseId.value,
-        source: selected.value
+        source: selected
     }
+}
+
+function getValueFromInput(id){
+    const element = document.querySelector(id);
+    const selected = element.value;
+
+    return selected;
 }
