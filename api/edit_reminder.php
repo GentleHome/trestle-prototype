0e4ba277d0060890cb7ce36e9728351eb20b1ce2
YourAlly <Royettemiranda@YAHOO.COM>
2021-12-12 19:27:04 +0800

    Fixed editing logic for reminders

--- api/edit_reminder.php
+++ api/edit_reminder.php
@@ -15,33 +15,13 @@ if (!isset($_POST['reminder-id'])) {
     array_push($errors["errors"], ERROR_MISSING_VALUE . ": Reminder");
 }
 
-if (!isset($_POST['title'])) {
-    array_push($errors["errors"], ERROR_MISSING_VALUE . ": Title");
-}
-
-if (!isset($_POST['remind-date'])) {
-    array_push($errors["errors"], ERROR_MISSING_VALUE . ": Remind Date");
-}
-
 if (empty($errors['errors'])) {
 
     $lookups = array();
     $user_id = !TEST_MODE ? $_SESSION[USER_ID] : 1;
     $reminder_id = $_POST['reminder-id'];
-    $title = $_POST['title'];
-    $type = $_POST['type'];
-    $remind_date = new DateTime($_POST['remind-date'], new DateTimeZone('Asia/Manila'));
-
     $user = $entityManager->find("User", $user_id);
     $reminder = $entityManager->find("Reminder", $reminder_id);
-
-    if ($type != TYPE_TASK && $type != TYPE_REMINDER) {
-
-        array_push($errors["errors"], ERROR_INVALID_VALUE . ": TYPE");
-        echo json_encode($errors);
-        exit;
-
-    }
 
     if (!$reminder->get_user()->get_id() == $user->get_id()){
         array_push($errors["errors"], ERROR_INVALID_ACCESS);
@@ -49,21 +29,27 @@ if (empty($errors['errors'])) {
         exit;
     }
 
-    $reminder->set_type($type);
-    $reminder->set_user($user);
-    $reminder->set_title($title);
+    if (isset($_POST['remind-date'])) {
+        $remind_date = new DateTime($_POST['remind-date'], new DateTimeZone('Asia/Manila'));
+        $reminder->set_remind_date($remind_date);
+    }
+
+
+    if (isset($_POST['title'])) {
+        $title = $_POST['title'];
+        $reminder->set_title($title);
+    }
+
 
     if (isset($_POST['message'])) {
         $message = $_POST['message'];
         $reminder->set_message(empty($message) ? null : $message);
-    } else {
-        $reminder->set_remind_date($remind_date);
     }
 
     $entityManager->persist($reminder);
     $entityManager->flush();
 
-    echo json_encode(array("success" => $type . " edited for ". $remind_date->format("M-d-Y h:i"), "reminder" => parse_reminder($reminder)));
+    echo json_encode(array("success" => $type . " edited", "reminder" => parse_reminder($reminder)));
 
 
 } else {
