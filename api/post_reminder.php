249493783f2c62d7c1c0d5882c1f7e68db3f891f
YourAlly <Royettemiranda@YAHOO.COM>
2021-12-20 23:09:37 +0800

    Bug fix attempt

--- api/post_reminder.php
+++ api/post_reminder.php
@@ -74,7 +74,7 @@ if (empty($errors['errors'])) {
     $entityManager->persist($reminder);
     $entityManager->flush();
 
-    echo json_encode(array("success" => $type . " created for ". $remind_date->format("M-d-Y h:i"), "reminder" => parse_reminder($reminder)));
+    echo json_encode(array("success" => $type . " created", "reminder" => parse_reminder($reminder)));
 
 } else {
 
