1046ddc18c8443a7e8e2a09115a14c3661c4e69e
YourAlly <Royettemiranda@YAHOO.COM>
2021-12-21 21:46:27 +0800

    Edited Login and Register
    They now return JSON as a response

--- api/login.php
+++ api/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__) . "/./bootstrap.php";
-require_once dirname(__FILE__) . '/./api/helpers/constants.php';
+require_once dirname(__FILE__) . "/../bootstrap.php";
+require_once dirname(__FILE__) . '/helpers/constants.php';
 session_start();
 
 $errors = [];
@@ -9,43 +9,32 @@ $messages = [];
 if (!isset($_POST['username'])) {
     array_push($errors, "Username input is missing");
 }
+
 if (!isset($_POST['password'])) {
     array_push($errors, "Password input is missing");
 }
 
 if(!count($errors) === 0){
-    
-    $_SESSION[ERRORS] = $errors;
-    header("Location: ./forms.php");
+    echo json_encode(array("errors" => $errors));
     exit();
-
-} else {
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 }
+
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $user = $entityManager->getRepository('User')->findOneBy(array('username' => $username));
 
 if (!$user) {
-
     array_push($errors, "User not found");
-
 } else if ($user->authenticate($password)) {
-
     $_SESSION[USER_ID] = $user->get_id();
 
     array_push($messages, "Login Successful!");
-    $_SESSION[MESSAGES] = $messages;
-
-    header("Location: ./api/tests.php");
-    exit();
-
-} else {
-
-    array_push($errors, "Authentication Error");
+    echo json_encode(array("messages" => $messages));
     
+    exit();
+} else {
+    array_push($errors, "Authentication Error");
 }
 
-$_SESSION[ERRORS] = $errors;
-header("Location: ./forms.php");
+echo json_encode(array("errors"=> $errors));
