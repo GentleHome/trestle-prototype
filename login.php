e68b464e9d8a233dc41986069ad235ad039f35d2
YourAlly <Royettemiranda@YAHOO.COM>
2021-12-22 23:58:54 +0800

    Cleaned up again

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 require_once dirname(__FILE__) . "/./bootstrap.php";
-require_once dirname(__FILE__) . './api/helpers/constants.php';
+require_once dirname(__FILE__) . '/./api/helpers/constants.php';
 session_start();
 
 $errors = [];
