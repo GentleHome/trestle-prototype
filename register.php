94e48bca25bf8ccd2d410c2331b3f4ede3486755
Albert-Jorda <92785696+Albert-Jorda@users.noreply.github.com>
2021-12-22 18:51:56 +0800

    log/reg

---
+++ register.php
@@ -0,0 +1,44 @@
+<?php
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+
+require_once dirname(__FILE__) . "/./bootstrap.php";
+require_once dirname(__FILE__) . './api/helpers/constants.php';
+session_start();
+
+$errors = [];
+$messages = [];
+
+if (!isset($_POST['username'])) {
+    array_push($errors, "Username input is missing");
+}
+if (!isset($_POST['password1'])) {
+    array_push($errors, "Password input is missing");
+}
+if (!isset($_POST['password2'])) {
+    array_push($errors, "Confirm Password input is missing");
+}
+
+$username = $_POST['username'];
+$password1 = $_POST['password1'];
+$password2 = $_POST['password2'];
+
+if($password1 === $password2){
+    $user = new User();
+    $user->set_username($username);
+    $user->set_password($password1);
+    try{
+        $entityManager->persist($user);
+        $entityManager->flush();
+        array_push($messages, "Registration Success");
+    }
+    catch(UniqueConstraintViolationException $e){
+        array_push($errors, "Username already exists!");
+    }
+}
+
+$_SESSION[MESSAGES] = $messages;
+$_SESSION[ERRORS] = $errors;
+
+
+header("Location: ./forms.php");
+
