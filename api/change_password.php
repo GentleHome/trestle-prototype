f6b478a4a3b2f3ae12d89ee628bd5b5bc0f5436c
RemovableDisk21 <logdat.bryanleo.a.0887@gmail.com>
2022-01-04 20:45:09 +0800

    Change Password Settings
    -Added a Password Settings form inside the settings.php
    -Change some response from change_password.php

--- api/change_password.php
+++ api/change_password.php
@@ -3,40 +3,29 @@ require_once dirname(__FILE__) . "/../bootstrap.php";
 require_once dirname(__FILE__) . '/./helpers/db_utils.php';
 require_once dirname(__FILE__) . '/helpers/constants.php';
 
-
+session_start();
 $user = get_logged_in_user($entityManager);
-
-if (!isset($_POST['current-password'])) {
-    array_push($errors, ERROR_MISSING_VALUE . ": Current Password");
-}
-
-if (!isset($_POST['new-password1'])) {
-    array_push($errors, ERROR_MISSING_VALUE . ": New Password");
-}
-
-if (!isset($_POST['new-password2'])) {
-    array_push($errors, ERROR_MISSING_VALUE . ": Confirm New Password");
-}
-
-if (!count($errors) === 0) {
+if (!isset($_POST['currentPassword']) || $_POST['currentPassword']==null || !isset($_POST['newPassword1']) || $_POST['newPassword1']==null || !isset($_POST['newPassword2']) || $_POST['newPassword2']==null) {
+    $errors = "All fields are required.";
     echo json_encode(array("errors" => $errors));
     exit();
 }
 
-$current_password = $_POST['current-password'];
-$password1 = $_POST['new-password1'];
-$password2 = $_POST['new-password2'];
+
+$current_password = $_POST['currentPassword'];
+$password1 = $_POST['newPassword1'];
+$password2 = $_POST['newPassword2'];
 
 if ($user->authenticate($current_password)) {
     if ($password1 == $password2) {
         $user->set_password($password1);
-        array_push($messages, "Password Change Success!");
+        $messages = "Password Change Success!";
         echo json_encode(array("messages" => $messages));
     } else {
-        array_push($errors, ERROR_INVALID_VALUE . ": Confirm New Password");
+        $errors = ERROR_INVALID_VALUE . ": Confirm New Password";
         echo json_encode(array("errors" => $errors));
     }
 } else {
-    array_push($errors, ERROR_INVALID_VALUE . ": Current Password");
+    $errors = ERROR_INVALID_VALUE . ": Current Password";
     echo json_encode(array("errors" => $errors));
 }
