203c11d9294a3787fcefcfc28106b947668fe328
YourAlly <Royettemiranda@YAHOO.COM>
2022-02-13 01:07:14 +0800

    Added new remember_me logic to login

--- api/login.php
+++ api/login.php
@@ -14,11 +14,14 @@ if (!isset($_POST['username']) || !isset($_POST['password']) || $_POST['username
 
 $username = $_POST['username'];
 $password = $_POST['password'];
+$remember_me = $_POST['remember_me'];
 
 $user = $entityManager->getRepository('User')->findOneBy(array('username' => $username));
 
 if ($user && $user->authenticate($password)) {
     $_SESSION[USER_ID] = $user->get_id();
+
+    if ($remember_me) session_set_cookie_params(time() + 60 * 60 * 24 * 30);
 
     $messages = "Login Successful!";
     echo json_encode(array("messages" => $messages));
